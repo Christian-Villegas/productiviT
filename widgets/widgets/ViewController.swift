bc59a71795790a677b5857bd4c8ba40e50df8fca
Christopher Cordero <cjcorder@usc.edu>
2020-07-05 18:51:58 -0700

    Latest File
    Has all work to do date

--- widgets/widgets/ViewController.swift
+++ widgets/widgets/ViewController.swift
@@ -8,34 +8,169 @@
 
 import UIKit
 
+//PlaceHolder Struct
 struct PlaceHolder {
-    var width = 207
-    var height = 192
-    var filled = false
+    var filled: Bool
+    var centerX, centerY: Double
+    var frame: CGRect
     
+    init(row: Int, column: Int){
+        self.filled = false
+        if (column % 2) == 0 {
+            self.centerX = 108.5
+        } else {
+            self.centerX = 305.5
+        }
+        self.centerY = 132.5 + Double(row * 185)
+        
+        self.frame = CGRect(x: self.centerX, y: self.centerY, width: 177, height: 177)
+    }
+}
+
+//Double Array of All the PlaceHolders that will be on screen
+struct placeHolderArray {
+    var grid = [[PlaceHolder]]()
+    
+    init(){
+        for row in (0...7){
+            var newPHA = Array<PlaceHolder>()
+            for column in (0...1){
+                newPHA.append(PlaceHolder(row:row, column:column))
+            }
+            grid.append(newPHA)
+        }
+    }
 }
 
 
+
+
+public var editOn = false
+var screenWidgets: [Widget] = []
+
 class ViewController: UIViewController {
 
+    var placeHolders = placeHolderArray()
+    
     
     var xC = 0
     var yC = 0
     
-    @IBAction func addWidget(_ panGesture: UIButton) {
+    let editButton = UIButton(type: .system) // let preferred over var here
+    let addWidgetButton = UIButton(type: .system)
+    
+    
+    @IBOutlet weak var widgetMenu: UITableView!
+    
+    @IBAction func addWidget(_ sender: UIButton) {
+        
+        if editOn == false{return}
+        
+        let xC = placeHolders.grid[0][0].centerX
+        let yC = placeHolders.grid[0][0].centerY
         let newWidget = Widget(frame: CGRect(x: xC, y: yC, width: 177, height: 177))
-        self.view.addSubview(newWidget)
+        
+        
+        placeNextWidget(PHA: &placeHolders.grid, addedWidget: newWidget)
+        //self.view.addSubview(newWidget)
+        
+        self.view.insertSubview(newWidget, belowSubview: widgetMenu)
+        
+        screenWidgets.append(newWidget)
+        
+    }
+    
+    func placeNextWidget(PHA: inout [[PlaceHolder]], addedWidget:Widget){
+        for row in (0...7){
+            for column in (0...1){
+                print("\(row) and \(column)")
+                //var slot = PHA[row][column]
+                print("Checking slot \(PHA[row][column]) which is \(PHA[row][column].filled)")
+                if (PHA[row][column].filled == false){
+                        addedWidget.center = CGPoint(x: PHA[row][column].centerX, y: PHA[row][column].centerY)
+                        //print(slot.centerX)
+                        //print(slot.centerY)
+                        PHA[row][column].filled = true
+                        print(PHA[row][column].filled)
+                        return
+                }
+            }
+        }
+    }
+    
+    @IBAction func addSmartGoals(_ sender: UIButton) {
+        if editOn == false{return}
+        
+        let smartWidget = SmartGoal(frame: CGRect(x: xC, y: yC, width: 177, height: 177))
+        self.view.insertSubview(smartWidget, belowSubview: widgetMenu)
+        screenWidgets.append(smartWidget)
+        print(screenWidgets.count)
         
         xC += 50
         yC += 50
-        
     }
+    
+    @objc func editHome(sender: UIButton!) {
+        if editOn == false {editOn = true}
+        else {editOn = false}
+        if editOn == true {
+            editButton.setTitle("done", for: .normal)
+            addWidgetButton.isHidden = false
+            
+            if screenWidgets.count > 0{
+                for i in 0...(screenWidgets.count-1) {
+                    screenWidgets[i].delButton.isHidden = false
+                    screenWidgets[i].sizeButton.isHidden = false
+                }
+            }
+        }
+        else {
+            editButton.setTitle("edit", for: .normal)
+            addWidgetButton.isHidden = true
+            widgetMenu.isHidden = true
+            addWidgetButton.isHidden = true
+            addWidgetButton.setTitle("+", for: .normal)
+            addWidgetButton.frame = CGRect(x: 20, y: 44, width: 40, height: 25)
+            if screenWidgets.count > 0{
+                for i in 0...(screenWidgets.count-1) {
+                    screenWidgets[i].delButton.isHidden = true
+                    screenWidgets[i].sizeButton.isHidden = true
+                }
+            }
+        }
+    }
+    
+    @objc func plusButton(sender: UIButton!) {
+        if widgetMenu.isHidden == true {
+            widgetMenu.isHidden = false
+            addWidgetButton.setTitle("close", for: .normal)
+            addWidgetButton.frame = CGRect(x: 263, y: 44, width: 40, height: 25)
+        }
+        else{
+            widgetMenu.isHidden = true
+            addWidgetButton.setTitle("+", for: .normal)
+            addWidgetButton.frame = CGRect(x: 20, y: 44, width: 40, height: 25)
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //print(widgetOne.center)
-        
+        editButton.frame = CGRect(x: 348, y: 44, width: 40, height: 25)
+        editButton.setTitle("edit", for: .normal)
+        editButton.contentHorizontalAlignment = .right
+        editButton.addTarget(self, action: #selector(self.editHome), for: UIControl.Event.touchUpInside)
+        self.view.addSubview(editButton)
+        widgetMenu.frame = CGRect(x: 0, y: 44, width: 259, height: 769)
+        widgetMenu.isHidden = true
+        addWidgetButton.setTitle("+", for: .normal)
+        addWidgetButton.frame = CGRect(x: 20, y: 44, width: 40, height: 25)
+        editButton.contentHorizontalAlignment = .left
+        addWidgetButton.addTarget(self, action: #selector(self.plusButton), for: UIControl.Event.touchUpInside)
+        addWidgetButton.isHidden = true
+        self.view.addSubview(addWidgetButton)
     }
     
     
--- widgets/widgets/Widget.swift
+++ widgets/widgets/Widget.swift
@@ -22,24 +22,56 @@ class Widget: UIView {
         self.title = ""
         super.init(frame: frame)
         self.backgroundColor = .lightGray
-        // transparency
         self.alpha = 0.5
-        // makes the corners round
         self.layer.cornerRadius = 20
         self.layer.masksToBounds = true
-        // to move widget with finger
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(self.panView))
         self.addGestureRecognizer(panGesture)
         
-        let button = UIButton(frame: CGRect(x: 20.0, y: 20.0, width: 40.0, height: 40.0))
-        button.backgroundColor = .black
-        button.setTitle("Test", for: .normal)
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        sizeButton.setTitle("change size", for: .normal)
+        sizeButton.addTarget(self, action: #selector(changeSize), for: .touchUpInside)
+        self.addSubview(sizeButton)
         
-        self.addSubview(button)
+        
+        delButton.setTitle("x", for: .normal)
+        delButton.addTarget(self, action: #selector(deleteWidget), for: .touchUpInside)
+        self.addSubview(delButton)
     }
     
-    @objc func buttonAction(sender: UIButton) {
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func deleteWidget(sender: UIButton) {
+        
+        if editOn == false {return}//returns if not in edit mode
+        
+        let alert = UIAlertController(title: "Are you sure?", message: "Deleting this widget will also delete all of its data.", preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: {action in
+              self.removeFromSuperview()
+        }))
+        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+        
+        
+        let keyWindow = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+
+        if var topController = keyWindow?.rootViewController {
+            while let presentedViewController = topController.presentedViewController {
+                topController = presentedViewController
+            }
+            topController.present(alert, animated: true, completion: nil)
+        }
+    
+        
+
+        
+        
+    }
+    
+    @objc func changeSize(sender: UIButton) {
+        if editOn == false {return}//returns if not in edit mode
+        
         if(self.frame.width == 177.0) {
             self.frame = CGRect(x: self.frame.minX, y: self.frame.minY, width: 374.0, height: 177.0)
         } else if(self.frame.width == 374.0 && self.frame.height == 177.0) {
@@ -50,55 +82,55 @@ class Widget: UIView {
         
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     @objc func panView(_ panGesture: UIPanGestureRecognizer) {
-   
-       // print("x")
-        let translation = panGesture.translation(in: self)
+            //
+        
+        if editOn == false {return}//returns if not in edit mode
+        
+            let translation = panGesture.translation(in: self)
 
         if let viewToDrag = panGesture.view {
-            viewToDrag.center = CGPoint(x: viewToDrag.center.x + translation.x,
-                y: viewToDrag.center.y + translation.y)
-            panGesture.setTranslation(CGPoint(x: 0, y: 0), in: viewToDrag)
-        }
+                viewToDrag.center = CGPoint(x: viewToDrag.center.x + translation.x,
+                    y: viewToDrag.center.y + translation.y)
+                panGesture.setTranslation(CGPoint(x: 0, y: 0), in: viewToDrag)
+                
+            }
+            //
             
-        print("x \(translation.x)")
-        print("y \(translation.y)")
-        
-        
-        if (abs(108.5 - panGesture.view!.center.x) <= 98.5 && abs(132.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
-            panGesture.view?.center = CGPoint(x: 108.5, y: 132.5)
-        }
-        else if (abs(305.5 - panGesture.view!.center.x) <= 98.5 && abs(132.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
-            panGesture.view?.center = CGPoint(x: 305.5, y: 132.5)
-        }
-        else if (abs(108.5 - panGesture.view!.center.x) <= 98.5 && abs(317.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
-            panGesture.view?.center = CGPoint(x: 108.5, y: 317.5)
-        }
-        else if (abs(305.5 - panGesture.view!.center.x) <= 98.5 && abs(317.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
-            panGesture.view?.center = CGPoint(x: 305.5, y: 317.5)
-        }
-        else if (abs(108.5 - panGesture.view!.center.x) <= 98.5 && abs(502.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
-            panGesture.view?.center = CGPoint(x: 108.5, y: 502.5)
-        }
-        else if (abs(305.5 - panGesture.view!.center.x) <= 98.5 && abs(502.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
-            panGesture.view?.center = CGPoint(x: 305.5, y: 502.5)
-        }
-        else if (abs(108.5 - panGesture.view!.center.x) <= 98.5 && abs(687.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
-            panGesture.view?.center = CGPoint(x: 108.5, y: 687.5)
-        }
-        else if (abs(305.5 - panGesture.view!.center.x) <= 98.5 && abs(687.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
-            panGesture.view?.center = CGPoint(x: 305.5, y: 687.5)
-        }
+            
+            if (abs(108.5 - panGesture.view!.center.x) <= 98.5 && abs(132.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
+                panGesture.view?.center = CGPoint(x: 108.5, y: 132.5)
+            }
+            else if (abs(305.5 - panGesture.view!.center.x) <= 98.5 && abs(132.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
+                panGesture.view?.center = CGPoint(x: 305.5, y: 132.5)
+            }
+            else if (abs(108.5 - panGesture.view!.center.x) <= 98.5 && abs(317.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
+                panGesture.view?.center = CGPoint(x: 108.5, y: 317.5)
+            }
+            else if (abs(305.5 - panGesture.view!.center.x) <= 98.5 && abs(317.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
+                panGesture.view?.center = CGPoint(x: 305.5, y: 317.5)
+            }
+            else if (abs(108.5 - panGesture.view!.center.x) <= 98.5 && abs(502.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
+                panGesture.view?.center = CGPoint(x: 108.5, y: 502.5)
+            }
+            else if (abs(305.5 - panGesture.view!.center.x) <= 98.5 && abs(502.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
+                panGesture.view?.center = CGPoint(x: 305.5, y: 502.5)
+            }
+            else if (abs(108.5 - panGesture.view!.center.x) <= 98.5 && abs(687.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
+                panGesture.view?.center = CGPoint(x: 108.5, y: 687.5)
+            }
+            else if (abs(305.5 - panGesture.view!.center.x) <= 98.5 && abs(687.5 - panGesture.view!.center.y) <= 98.5 ) && (translation.x == 0.0 && translation.y == 0.0){
+                panGesture.view?.center = CGPoint(x: 305.5, y: 687.5)
+            }
         
         
     }
+    
     
     //Data Members://
     var title: String
+    let delButton = UIButton(frame: CGRect(x: 135, y: 4, width: 40.0, height: 40.0))
+     let sizeButton = UIButton(frame: CGRect(x: 20.0, y: 20.0, width: 100.0, height: 40.0))
     
     
     //Height var (From UIView Class)
