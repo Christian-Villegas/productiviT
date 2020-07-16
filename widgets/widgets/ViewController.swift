644e45ba8bbdad539435412554f889f407cba24f
Christian Ruben Villegas <53159807+Christian-Villegas@users.noreply.github.com>
2020-07-16 16:19:05 -0700

    Add files via upload

--- widgets/widgets/ViewController.swift
+++ widgets/widgets/ViewController.swift
@@ -11,112 +11,184 @@ import UIKit
 //PlaceHolder Struct
 struct PlaceHolder {
     var filled: Bool
-    var centerX, centerY: Double
+    // the position of a CGRect is top left
+    var posX, posY: Double
+    var xC, yC: Double
     var frame: CGRect
+    var widget: Widget?
+    var number: Int
     
-    init(row: Int, column: Int){
+    init(row: Int, column: Int) {
         self.filled = false
-        if (column % 2) == 0 {
-            self.centerX = 108.5
-        } else {
-            self.centerX = 305.5
-        }
-        self.centerY = 132.5 + Double(row * 185)
+        self.widget = nil
+        self.number = 0
         
-        self.frame = CGRect(x: self.centerX, y: self.centerY, width: 177, height: 177)
+        if((column % 2) == 0) {
+            self.posX = 20
+            self.xC = 108.5
+        } else {
+            self.posX = 217
+            self.xC = 305.5
+        }
+        self.posY = 44 + Double(row * 185)
+        self.yC = 132.5 + Double(row * 185)
+        self.frame = CGRect(x: self.posX, y: self.posY, width: 177, height: 177)
+    }
+    
+    
+    mutating func setNumber(number: Int){
+        self.number = number
+    }
+    
+    mutating func setEmpty(){
+        self.filled = false
+    }
+    
+    mutating func setFilled(){
+        self.filled = true
     }
 }
 
 //Double Array of All the PlaceHolders that will be on screen
 struct placeHolderArray {
+    
+    // creates an array that can hold more arrays
     var grid = [[PlaceHolder]]()
     
     init(){
-        for row in (0...7){
+        var number = 0
+        for row in (0...3){
+            // creates an array that will hold the placeholder and their positions
             var newPHA = Array<PlaceHolder>()
             for column in (0...1){
-                newPHA.append(PlaceHolder(row:row, column:column))
+                number += 1
+                var newPlaceHolder = PlaceHolder(row: row, column: column)
+                newPlaceHolder.setNumber(number: number)
+                newPHA.append(newPlaceHolder)
             }
+            // adds the new arrays for each column
             grid.append(newPHA)
         }
     }
-}
+    
+    // make this along with change size. aka make sure that if a widget size 1 is on the right column, it cant change size
+    mutating func updateFilled() {
+        for row in (0...3){
+        //iterate through columns
+            for column in (0...1){
+                // left column
+                if(column == 0) {
+                    // if its a size 1 check that box
+                    if(self.grid[row][column].widget != nil && self.grid[row][column].widget?.size == 1) {
+                        self.grid[row][column].filled = true
+                    // if its a size 2 check that box and the one to the right
+                    } else if(self.grid[row][column].widget != nil && self.grid[row][column].widget?.size == 2) {
+                        self.grid[row][column].filled = true
+                        self.grid[row][column+1].filled = true
+                    }
+                    //if row is less than or equal to 2
+                    if(row <= 2) {
+                        if(self.grid[row][column].widget != nil && self.grid[row][column].widget?.size == 3) {
+                            self.grid[row][column].filled = true
+                            self.grid[row][column+1].filled = true
+                            self.grid[row+1][column].filled = true
+                            self.grid[row+1][column+1].filled = true
+                        }
+                    }
+                // right column
+                } else {
+                    // the only option if its the right column is if its size 1
+                    if(self.grid[row][column].widget != nil && self.grid[row][column].widget?.size == 1) {
+                        self.grid[row][column].filled = true
+                    }
+                }
+            }
+        }
+        gridPrint()
+    }
+    
+    func gridPrint(){
+        var statement = ""
+        for row in (0...3){
+            var addition = ""
+            for column in (0...1){
+                if self.grid[row][column].filled == true{
+                    addition.append("X")
+                }else if self.grid[row][column].filled == false{
+                    addition.append("0")
+                }
+            }
+            addition.append("\n")
+            statement.append(addition)
+        }
+        print(statement)
+    }
 
+}
 
 //Global variables//
 public var editOn = false
+// holds all widgets
 var screenWidgets: [Widget] = []
 var placeHolders = placeHolderArray()
-///
 
 
 class ViewController: UIViewController {
 
-    
-    
-    
-    var xC = 0
-    var yC = 0
+    var centerX = 0
+    var centerY = 0
     
     let editButton = UIButton(type: .system) // let preferred over var here
     let addWidgetButton = UIButton(type: .system)
-    
-    
-    
-    
+
     @IBOutlet weak var widgetMenu: UITableView!
     
     @IBAction func addWidget(_ sender: UIButton) {
+        // the edit button
+        if editOn == false {return}
         
-        if editOn == false{return}
-        
-        let xC = placeHolders.grid[0][0].centerX
-        let yC = placeHolders.grid[0][0].centerY
-        let newWidget = Widget(frame: CGRect(x: xC, y: yC, width: 177, height: 177))
-        
-        
-        placeNextWidget(PHA: &placeHolders.grid, addedWidget: newWidget)
-        //self.view.addSubview(newWidget)
-        
-        self.view.insertSubview(newWidget, belowSubview: widgetMenu)
-        
-        screenWidgets.append(newWidget)
-        
+        // if there is any space at all
+        if self.hasNextSpot() {
+            let posX = placeHolders.grid[0][0].posX
+            let posY = placeHolders.grid[0][0].posY
+            let newWidget = Widget(frame: CGRect(x: posX, y: posY, width: 177, height: 177))
+            
+            // will find the next empty space and change the center of the new widget to that one
+            placeNextWidget(PHA: &placeHolders.grid, addedWidget: newWidget)
+            placeHolders.gridPrint()
+            self.view.insertSubview(newWidget, belowSubview: widgetMenu)
+            screenWidgets.append(newWidget)
+        }
     }
     
-    func placeNextWidget(PHA: inout [[PlaceHolder]], addedWidget:Widget){
-        for row in (0...7){
+    func placeNextWidget(PHA: inout [[PlaceHolder]], addedWidget: Widget){
+        for row in (0...3){
             for column in (0...1){
-                //print("\(row) and \(column)")
-                //var slot = PHA[row][column]
-                //print("Checking slot \(PHA[row][column]) which is \(PHA[row][column].filled)")
-                if (PHA[row][column].filled == false){
-                        addedWidget.center = CGPoint(x: PHA[row][column].centerX, y: PHA[row][column].centerY)
-                    addedWidget.ogPosition = CGPoint(x: PHA[row][column].centerX, y: PHA[row][column].centerY)
-                        //print(slot.centerX)
-                        //print(slot.centerY)
-                        PHA[row][column].filled = true
-                        //print(PHA[row][column].filled)
-                        return
+                if(PHA[row][column].filled == false) {
+                    addedWidget.center = CGPoint(x: PHA[row][column].xC, y: PHA[row][column].yC)
+                    addedWidget.ogPosition = CGPoint(x: PHA[row][column].posX, y: PHA[row][column].posY)
+                    PHA[row][column].filled = true
+                    addedWidget.ogCenter = addedWidget.center
+                    // puts the widget to the array of placeHolders it takes
+                    addedWidget.placeHoldersAccessed.append(PHA[row][column])
+                    PHA[row][column].widget = addedWidget
+                    return
                 }
             }
         }
     }
     
-    @IBAction func addToDo(_ sender: UIButton) {
+    
+    @IBAction func addSmartGoals(_ sender: UIButton) {
         if editOn == false{return}
         
-        let toDoWidget = ToDoWidget(frame: CGRect(x: xC, y: yC, width: 177, height: 177))
-        self.view.insertSubview(toDoWidget, belowSubview: widgetMenu)
-        screenWidgets.append(toDoWidget)
+        let smartWidget = SmartGoal(frame: CGRect(x: centerX, y: centerY, width: 177, height: 177))
+        self.view.insertSubview(smartWidget, belowSubview: widgetMenu)
+        screenWidgets.append(smartWidget)
+        print(screenWidgets.count)
         
-        placeNextWidget(PHA: &placeHolders.grid, addedWidget: toDoWidget)
-        //self.view.addSubview(newWidget)
-        
-        self.view.insertSubview(toDoWidget, belowSubview: widgetMenu)
-        
-        screenWidgets.append(toDoWidget)
-        
+        centerX += 50
+        centerY += 50
     }
     
     @objc func editHome(sender: UIButton!) {
@@ -160,6 +232,18 @@ class ViewController: UIViewController {
             addWidgetButton.setTitle("+", for: .normal)
             addWidgetButton.frame = CGRect(x: 20, y: 44, width: 40, height: 25)
         }
+    }
+    
+    func hasNextSpot() -> Bool{
+        for row in (0...3){
+            for column in (0...1){
+                if(placeHolders.grid[row][column].filled == false){
+                    return true
+                }
+            }
+        }
+        print("nope! No more space")
+        return false
     }
     
     
