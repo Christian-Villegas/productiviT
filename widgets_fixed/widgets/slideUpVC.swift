911d6b30fd801e63c9dd1addb394fb6ec545ae21
Christopher Cordero <cjcorder@usc.edu>
2020-07-18 00:44:03 -0700

    Fixed and Most Updated Version of Project

---
+++ widgets_fixed/widgets/slideUpVC.swift
@@ -0,0 +1,43 @@
+//
+//  slideUpVC.swift
+//  widgets
+//
+//  Created by Christopher Cordero on 7/12/20.
+//  Copyright © 2020 Christopher Cordero. All rights reserved.
+//
+
+import UIKit
+
+var currentWidget = 0
+
+class slideUpVC: UIViewController {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+   
+    
+    
+    @IBOutlet weak var topTitle: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        switch currentWidget {
+        case 1:
+            topTitle.text = "To Do List"
+            view.addSubview(taskW.fullView)
+            
+        default:
+            topTitle.text = "None"
+        }
+    
+    
+    }
+    
+
+}
